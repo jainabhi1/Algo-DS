fe9c4f983e2d7476d23922e7893e2ab1acb6398f
abhishek <jainabhishek304@gmail.com>
2017-06-14 02:59:11 +0530

    added queue using stacks

---
+++ queue/queue_using_stacks.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cstdio>
+#include <stack>
+using namespace std;
+class queue{
+	stack<int> en,de;
+public:
+	void enqueue(int key)
+	{
+		en.push(key);
+	}
+	void deque()
+	{
+		if(en.empty() && de.empty())
+			throw runtime_error("empty");
+		if(de.empty())
+		{
+			while(!en.empty())
+			{
+				de.push(en.top());
+				en.pop();
+			}
+		}
+		de.pop();
+	}
+	int front()
+	{
+		if(en.empty() && de.empty())
+			throw runtime_error("empty");
+		if(de.empty())
+		{
+			while(!en.empty())
+			{
+				de.push(en.top());
+				en.pop();
+			}
+		}
+
+		return de.top();
+	}
+	bool empty()
+	{
+		return en.empty() && de.empty();
+	}
+};
+int main()
+{
+	queue q;
+	for(int i=0;i<10;i++)
+		q.enqueue(i);
+
+	while(!q.empty())
+	{
+		cout<<q.front()<<" ";
+		q.deque();
+	}
+	/*0 1 2 3 4 5 6 7 8 9*/
+}
